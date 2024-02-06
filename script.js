ebb24336799a025b257e09c40a4e238113976ad3
Gabriel Samuel <samuelgab246@gmail.com>
2024-02-06 10:51:41 -0800

    advanced js

---
+++ script.js
@@ -0,0 +1,21 @@
+                            //  let + const
+const player = 'samuel';
+let experience = 100;
+let wizardLevel = false;
+
+if (experience > 90) {
+    let wizardLevel = true;
+};
+
+
+//                               const;
+
+const obj = {
+    player : 'samuel',
+    experience : 100,
+    wizardLevel : false
+
+};
+
+
+
